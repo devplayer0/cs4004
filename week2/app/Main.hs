67f13632f6b8e00532f3676f24e2f7c34a83742b
Jack O'Sullivan <jackos1998@gmail.com>
2020-10-19 15:33:50 +0100

    Complete week 2

--- week2/app/Main.hs
+++ week2/app/Main.hs
@@ -1,6 +1,81 @@
-module Main where
+import System.IO
 
-import Lib
+-- Do each of the actions, returning only the last one (blind operator used to
+-- pass IO along)
+f1 :: [IO a] -> IO a
+f1 (x:[]) = x
+f1 (x:xs) = x >> f1 xs
 
-main :: IO ()
-main = someFunc
+
+hello = [putChar 'h', putChar 'e', putChar 'l', putChar 'l', putChar 'o']
+
+-- prints hello using `f1` as described
+main2 = f1 actions
+    where actions = hello
+
+
+-- a and b remain thunks and are never actually evaluated (sugared syntax makes
+-- this unclear)
+-- Hi there is the only thing printed
+main3 = do
+         let a = f1 actions
+             b = if True then putChar 'a' else putChar 'b'
+         putStr "Hi there"
+       where actions = hello
+
+-- desugaring the do shows a and b never referenced in the `in`
+main3a =
+    let a = f1 actions
+        b = if True then putChar 'a' else putChar 'b'
+    in putStr "Hi there"
+    where actions = hello
+
+
+while :: IO Bool -> IO ()
+
+-- do the action, storing the boolean and then either recurse on a again or
+-- use return to give a an IO ()
+while a = do
+    ok <- a
+    if ok then while a else return ()
+
+-- desugared shows the bind and use of lambda to store `ok`
+whilea a = a >>=
+        \ok -> if ok then while a else return ()
+
+-- keep prompting until the user enters "yes"
+main4 = while $ do
+    putStr "say yes: "
+    hFlush stdout
+
+    l <- getLine
+    return $ l /= "yes"
+
+
+f2 :: [IO a] -> IO [a]
+f2 [] = return []
+
+-- do x and store the result in y, then recurse and return the cons
+f2 (x:xs) = do
+    y <- x
+    ys <- f2 xs
+    return $ y:ys
+
+-- desugaring shows the bindings
+f2a (x:xs) = x >>=
+        \y -> f2 xs >>=
+            \ys -> return $ y:ys
+
+read10 :: IO String
+read10 = f2 $ take 10 actions
+          where actions = getChar : actions
+
+main5 = do
+    putStr "give me 10 chars: "
+    hFlush stdout
+
+    s <- read10
+    putStr "you said: "
+    putStrLn s
+
+main = main5
--- week2/src/Lib.hs
+++ week2/src/Lib.hs
@@ -2,5 +2,14 @@ module Lib
     ( someFunc
     ) where
 
+type World = ()
+type IO' a = World -> (a, World)
+
+(>>==) :: IO' a -> (a -> IO' b) -> IO' b
+(>>==) l r = \w -> let (x, w1) = l w in r x w1
+
+(>>>) :: IO' a -> IO' b -> IO' b
+(>>>) l r = l >>== (\_ -> r)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
