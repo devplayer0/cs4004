f42ebd7ddbfd6b916e9720c980dbd2b788c98319
Jack O'Sullivan <jackos1998@gmail.com>
2020-11-02 00:11:03 +0000

    Week 5 exercise 3

--- week5/src/Render.hs
+++ week5/src/Render.hs
@@ -1,4 +1,4 @@
-module Render(Window,defaultWindow,samples,render) where
+module Render(Window,defaultWindow,sample,render) where
 import Codec.Picture
 import Shapes
 
@@ -7,47 +7,33 @@ import Shapes
 --  resolution.
 --  Values are top left & bottom right corner to be rendered,
 --             and the size of the output device to render into
-data Window = Window Point Point (Int,Int)
+data Window = Window Point Point (Int, Int)
 
 -- Default window renders a small region around the origin into
 -- a 50x50 pixel image
 defaultWindow :: Window
-defaultWindow = Window (point (-1.5) (-1.5)) (point 1.5 1.5) (50,50)
+defaultWindow = Window (point (-1.5) (-1.5)) (point 1.5 1.5) (512, 512)
 
+-- Generate an evenly spaced sample between two values (at the given position)
+-- e.g. sample -1.5 +1.5 25 2 = -1.25
+sample :: Double -> Double -> Int -> Int -> Double
+sample c0 c1 n i = c0 + ((c1-c0) / (fromIntegral $ n-1) * fromIntegral i)
 
--- Generate a list of evenly spaced samples between two values.
--- e.g. samples -1.5 +1.5 25 generates 25 samples evenly spaced
---      between the two bounds: [ -1.5, -1.375, -1.25 ... ]
-samples :: Double -> Double -> Int -> [Double]
-samples c0 c1 n = take n [ c0, c0 + (c1-c0) / (fromIntegral $ n-1) .. ]
-
--- Generate the matrix of points corresponding to the pixels of a window.
-pixels :: Window -> [[Point]]
-pixels (Window p0 p1 (w,h)) =
-  [ [ point x y | x <- samples (getX p0) (getX p1) w ]
-                | y <- reverse $ samples (getY p0) (getY p1) h
-  ]
-
--- generate list of all screen coordinates in window
-coords :: Window -> [[(Int,Int)]]
-coords (Window _ _ (w,h)) = [ [(x,y) | x <- [0..w]] | y <- [0..h] ]
+-- Generate a point corresponding to the pixel of a window.
+pixel :: Window -> (Int, Int) -> Point
+pixel (Window p0 p1 (w, h)) (i, j) =
+  point x y where
+    x = sample (getX p0) (getX p1) w i
+    y = sample (getY p0) (getY p1) h j
 
 -- render a drawing into an image, then save into a file
--- NB: the lookup1 function is a VERY inefficient way to convert screen coordinates to drawing
---     coordinates! It should be possible to do this in O(1) time, not O(N) time!!
 render :: String -> Window -> Drawing -> IO ()
 render path win sh = writePng path $ generateImage pixRenderer w h
     where
       Window _ _ (w,h) = win
 
-      pixRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) (colorForImage $ lookup1 (x,y) locations)
+      pixRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) (blueForPoint $ pixel win (x, y))
 
-      lookup1 :: (Int,Int) -> [((Int,Int), Point)] -> Point
-      lookup1 a m = case lookup a m of
-                      Nothing -> point 0 0
-                      Just x  -> x
-
-      locations :: [ ((Int,Int), Point) ]
-      locations = concat $ zipWith zip (coords win) (pixels win)
-      colorForImage p | p `inside` sh = 255
-                      | otherwise     = 0
+      blueForPoint :: Point -> Pixel8
+      blueForPoint p | p `inside` sh = 255
+                     | otherwise     = 0
